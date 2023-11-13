b899a335f8966c9535cd6bde578c34ac2a5f78fd
mosidrum <mosiokanga@gmail.com>
2023-11-13 13:47:33 +0100

    clean up

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors({
   origin: ["http://localhost:3000"],
   methods: ["POST", "DELETE", "GET", "PUT"],
-  withCredentials: true
+  credentials: true
 }));
 app.use(cookieParser());
 
